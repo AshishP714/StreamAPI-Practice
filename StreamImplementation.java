912fcdf9565249f25977d173602e276e16bb8725
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-10 13:36:22 +0530

    Add files via upload

---
+++ StreamImplementation.java
@@ -0,0 +1,29 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamImplementation {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		
+		l.add(0);
+		l.add(10);
+		l.add(15);
+		l.add(20);
+		l.add(5);
+		l.add(2);
+		l.add(8);
+		l.add(4);
+		System.out.println("Before stream");
+		System.out.println(l);
+		
+		List<Integer> l1 = l.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+		
+		System.out.println("After stream");
+		System.out.println(l1);
+	}
+}
